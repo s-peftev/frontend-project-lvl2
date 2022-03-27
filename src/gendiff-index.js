9cc87d2b6de56634e14e427a06faecef9b6c2fad
s-peftev <s.peftev@gmail.com>
2022-03-28 00:37:06 +0300

    step 6: code climate issues fixing

--- src/gendiff-index.js
+++ src/gendiff-index.js
@@ -69,22 +69,22 @@ const getFormarter = (formarterName) => {
   return FORMARTER_MAP[formarterName];
 };
 
-const getDifference = (data1, data2) => {
+const getDifference = (data1, data2, type) => {
   const diff = {};
   const entriesData1 = Object.entries(data1);
-  const entriesData2 = Object.entries(data2);
 
   _.map(entriesData1, ([key, value]) => {
     if (!Object.hasOwn(data2, key)) {
-      diff[key] = { type: 'deleted', value };
+      diff[key] = { type, value };
     }
   });
 
-  _.map(entriesData2, ([key, value]) => {
-    if (!Object.hasOwn(data1, key)) {
-      diff[key] = { type: 'added', value };
-    }
-  });
+  return diff;
+};
+
+const getIntersection = (data1, data2, callback) => {
+  const diff = {};
+  const entriesData1 = Object.entries(data1);
 
   _.map(entriesData1, ([key, value]) => {
     if (Object.hasOwn(data2, key)) {
@@ -97,7 +97,7 @@ const getDifference = (data1, data2) => {
       if (_.isObject(value) && _.isObject(data2[key])) {
         diff[key] = {
           type: 'changed',
-          children: getDifference(value, data2[key]),
+          children: callback(value, data2[key]),
         };
       }
     }
@@ -106,11 +106,19 @@ const getDifference = (data1, data2) => {
   return diff;
 };
 
+const getDiffReport = (data1, data2) => {
+  const deleted = getDifference(data1, data2, 'deleted');
+  const added = getDifference(data2, data1, 'added');
+  const intersected = getIntersection(data1, data2, getDiffReport);
+
+  return { ...deleted, ...added, ...intersected };
+};
+
 export default (file1, file2, formarterName = 'stylish') => {
   const fileData1 = getParsedData(file1);
   const fileData2 = getParsedData(file2);
 
-  const diff = getDifference(fileData1, fileData2);
+  const diff = getDiffReport(fileData1, fileData2);
 
   const formarter = getFormarter(formarterName);
 
