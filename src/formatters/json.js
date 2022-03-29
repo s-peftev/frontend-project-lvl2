c248bb6a543859bf846867128a13c59fb48da94a
s-peftev <s.peftev@gmail.com>
2022-03-29 17:49:04 +0300

    fix issues

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,9 +2,6 @@ import _ from 'lodash';
 
 const complexValuePlaceholder = '[complex value]';
 
-const isDeleted = (diffBody) => diffBody.type === 'deleted';
-const isAdded = (diffBody) => diffBody.type === 'added';
-const isUpdated = (diffBody) => diffBody.type === 'updated';
 const getStyledValue = (value) => {
   if (typeof value === 'string') {
     return `"${value}"`;
@@ -13,44 +10,47 @@ const getStyledValue = (value) => {
   return value;
 };
 
-const renderDeleted = (parrent, key) => `{"propertyName":"${parrent}${key}","diffType":"deleted", "value":""}`;
+const renderDeleted = (body, parrent, key) => `{"propertyName":"${parrent}${key}","diffType":"${body.type}", "value":""}`;
 
 const renderAdded = (body, parrent, key) => {
   const value = _.isObject(body.value) ? complexValuePlaceholder : body.value;
 
-  return `{"propertyName":"${parrent}${key}","diffType":"added", "value":${getStyledValue(value)}}`;
+  return `{"propertyName":"${parrent}${key}","diffType":"${body.type}", "value":${getStyledValue(value)}}`;
 };
 
-const renderUpdated = (body, parrent, key) => {
-  const changed = body.value;
-  const fromValue = _.isObject(changed.fromValue)
-    ? complexValuePlaceholder
-    : changed.fromValue;
-  const toValue = _.isObject(changed.toValue)
-    ? complexValuePlaceholder
-    : changed.toValue;
+const renderUpdated = (body, parrent, key, callback) => {
+  if (_.has(body.value, 'fromValue')) {
+    const changed = body.value;
+    const fromValue = _.isObject(changed.fromValue)
+      ? complexValuePlaceholder
+      : changed.fromValue;
+    const toValue = _.isObject(changed.toValue)
+      ? complexValuePlaceholder
+      : changed.toValue;
 
-  return `{"propertyName":"${parrent}${key}","diffType":"updated", "value":{"fromValue":${getStyledValue(fromValue)},"toValue":${getStyledValue(toValue)}}}`;
+    return `{"propertyName":"${parrent}${key}","diffType":"${body.type}", "value":{"fromValue":${getStyledValue(fromValue)},"toValue":${getStyledValue(toValue)}}}`;
+  }
+
+  const newParrent = parrent === '' ? `${key}.` : `${parrent}${key}.`;
+  return callback(body.value, newParrent);
+};
+
+const TYPE_RENDER_MAP = {
+  deleted: (body, parrent, key) => renderDeleted(body, parrent, key),
+  added: (body, parrent, key) => renderAdded(body, parrent, key),
+  updated: (body, parrent, key, callback) => renderUpdated(body, parrent, key, callback),
+  same: () => null,
+  nestedComlex: () => null,
 };
 
 const jsonFormarter = (diffReport) => {
   const getFormattedString = (diff, parrent = '') => {
     const sorted = _.sortBy(Object.entries(diff), ([key]) => key);
     const maped = sorted.flatMap(([key, body]) => {
-      if (isDeleted(body)) {
-        return renderDeleted(parrent, key);
-      }
-      if (isAdded(body)) {
-        return renderAdded(body, parrent, key);
-      }
-      if (isUpdated(body)) {
-        if (_.has(body.value, 'fromValue')) {
-          return renderUpdated(body, parrent, key);
-        }
-        const newParrent = parrent === '' ? `${key}.` : `${parrent}${key}.`;
-        return getFormattedString(body.value, newParrent);
-      }
-      return null;
+      const resultRender = _.has(body, 'value')
+        ? TYPE_RENDER_MAP[body.type]
+        : TYPE_RENDER_MAP.nestedComlex;
+      return resultRender(body, parrent, key, getFormattedString);
     });
 
     return maped.filter((notNull) => notNull).join(',');
