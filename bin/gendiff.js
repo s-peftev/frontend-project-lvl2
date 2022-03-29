587f6d03fdcb330addf376f1030bbc8a2c6feed0
s-peftev <s.peftev@gmail.com>
2022-03-29 15:57:51 +0300

    fix issues

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { writeFileSync } from 'fs';
 import compareFiles from '../src/gendiff-index.js';
 
 const program = new Command();
@@ -13,10 +12,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    /* const actualStylish = readFileSync('__fixtures__/result_plain.txt', 'utf-8');
-    console.log(actualStylish); */
     console.log(compareFiles(filepath1, filepath2, program.opts().format));
-    // writeFileSync('__fixtures__/result_plain1.txt', compareFiles(filepath1, filepath2, program.opts().format));
   });
 
 program.parse(process.argv);
